8d28294e8511cee3ce67182ee9daed3627bb3cfc
xianren <xianren711@qq.com>
2024-07-18 15:18:56 +0800

    first

---
+++ exercises/02_functions/functions1.rs
@@ -0,0 +1,10 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+
+fn main() {
+    call_me(); // Don't change this line
+}
+
+fn call_me() {
+    println!("I called the function!");
+    // TODO: Call the function here
+}
