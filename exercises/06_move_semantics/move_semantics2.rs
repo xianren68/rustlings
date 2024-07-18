2d86a4c2ea030f97b90b20d63e0268421e20853e
xianren <xianren711@qq.com>
2024-07-18 15:55:43 +0800

    update

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -19,10 +19,9 @@ mod tests {
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
-
+        assert_eq!(vec0, [22, 44, 66]);
         let vec1 = fill_vec(vec0);
 
-        assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
     }
 }
--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -15,7 +15,13 @@
 fn main() {
     // You can optionally experiment here.
 }
-
+fn calculate_price_of_apples(apples: u32) -> u32 {
+    if apples > 40 {
+        apples
+    } else {
+        apples * 2
+    }
+}
 // Don't change the tests!
 #[cfg(test)]
 mod tests {
